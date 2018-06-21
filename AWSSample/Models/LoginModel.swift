a82520915ec67c3760c75720ce569fa51e74e80f
KoingDev <>
2018-06-21 17:03:48 +0700

    Update LoginModel

--- AWSSample/Models/LoginModel.swift
+++ AWSSample/Models/LoginModel.swift
@@ -11,20 +11,7 @@ import Foundation
 final class LoginModel {
     
     func login(username: String, password: String, completion: @escaping (Error?) -> Void) {
-        let user = CognitoUserPoolManager.sharedInstance.userPool?.getUser(username)
-        let task = user?.getSession(username, password: password, validationData: nil)
-        task?.continueWith { task in
-            // Error
-            if let error = task.error {
-                completion(error)
-                return nil
-            }
-            // Success
-            CognitoUserPoolManager.sharedInstance.author = username
-            cognitoToken = task.result?.accessToken?.tokenString ?? ""
-            completion(nil)
-            return nil
-        }
+        CognitoUserPoolManager.sharedInstance.login(username: username, password: password, completion: completion)
     }
     
 }
